ce4766ba245946f0ce6937a962fe45fe26bf4e5e
teja <pteja1021@gmail.com>
2022-09-28 12:12:20 +0530

    added hasura

---
+++ hasura/exercise/migrations/default/1664345691213_checking_username/down.sql
@@ -0,0 +1,17 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION CHECK_USERNAME()
+-- RETURNS TRIGGER
+-- AS $BODY$
+-- BEGIN
+-- IF LENGTH(NEW.NAME) < 4 THEN
+--     raise exception 'length cant be less than 4';
+-- ELSE RETURN NEW;
+-- END IF;
+-- END;
+-- $BODY$ LANGUAGE plpgsql;
+--
+-- CREATE OR REPLACE TRIGGER CHECK_USER_NAME
+-- BEFORE INSERT ON "public"."users"
+-- FOR EACH ROW
+-- EXECUTE PROCEDURE CHECK_USERNAME();
